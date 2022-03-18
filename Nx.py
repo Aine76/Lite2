cb490d60df1d6befeca100811498cee00e61e37a
Qurat677 <101870036+Qurat677@users.noreply.github.com>
2022-03-18 15:52:11 +0500

    Update Nx.py

--- Nx.py
+++ Nx.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from bboy import xsxsxlll
+    from qu import xsxsxlll
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     xsxsxlll()
